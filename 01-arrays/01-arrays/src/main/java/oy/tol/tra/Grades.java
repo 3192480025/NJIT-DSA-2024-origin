f45dde4051b1ba0eedd9de033fec674befe3fc32
KAIWEN ZHANG <126432067+3192480025@users.noreply.github.com>
2024-03-24 22:47:08 +0800

    Add files via upload

---
+++ 01-arrays/01-arrays/src/main/java/oy/tol/tra/Grades.java
@@ -0,0 +1,43 @@
+package oy.tol.tra;
+
+public class Grades<T extends Comparable<T>> {
+
+   private T[] grades = null;
+
+   public Grades(T[] grades) {
+      this.grades = grades.clone();
+   }
+
+   public void reverse() {
+      int start = 0;
+      int end = grades.length - 1;
+
+      while (start < end) {
+         T temp = grades[start];
+         grades[start] = grades[end];
+         grades[end] = temp;
+
+         start++;
+         end--;
+      }
+   }
+
+   public void sort() {
+      boolean swapped;
+      do {
+         swapped = false;
+         for (int i = 1; i < grades.length; i++) {
+            if (grades[i - 1].compareTo(grades[i]) > 0) {
+               T temp = grades[i - 1];
+               grades[i - 1] = grades[i];
+               grades[i] = temp;
+               swapped = true;
+            }
+         }
+      } while (swapped);
+   }
+
+   public T[] getArray() {
+      return grades.clone();
+   }
+}
