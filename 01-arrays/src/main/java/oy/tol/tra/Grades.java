6fcdedba084c9a5e3c022dd9263038667bf34ef0
Xiangplusss <1935274145@qq.com>
2024-03-26 13:56:59 +0800

    changed

--- 01-arrays/src/main/java/oy/tol/tra/Grades.java
+++ 01-arrays/src/main/java/oy/tol/tra/Grades.java
@@ -9,32 +9,11 @@ public class Grades<T extends Comparable<T>> {
    }
 
    public void reverse() {
-      int start = 0;
-      int end = grades.length - 1;
-
-      while (start < end) {
-         T temp = grades[start];
-         grades[start] = grades[end];
-         grades[end] = temp;
-
-         start++;
-         end--;
-      }
+      Algorithms.reverse(grades);
    }
 
    public void sort() {
-      boolean swapped;
-      do {
-         swapped = false;
-         for (int i = 1; i < grades.length; i++) {
-            if (grades[i - 1].compareTo(grades[i]) > 0) {
-               T temp = grades[i - 1];
-               grades[i - 1] = grades[i];
-               grades[i] = temp;
-               swapped = true;
-            }
-         }
-      } while (swapped);
+      Algorithms.sort(grades);
    }
 
    public T[] getArray() {
