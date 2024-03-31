7f09e7aba4bf7862f2ae16f0d7603214ecb4ec2a
KAIWEN ZHANG <126432067+3192480025@users.noreply.github.com>
2024-03-31 21:12:18 +0800

    Update Grades.java

--- 01-arrays/01-arrays/src/main/java/oy/tol/tra/Grades.java
+++ 01-arrays/01-arrays/src/main/java/oy/tol/tra/Grades.java
@@ -5,39 +5,18 @@ public class Grades<T extends Comparable<T>> {
    private T[] grades = null;
 
    public Grades(T[] grades) {
-      this.grades = grades.clone();
+      this.grades = grades;
    }
 
    public void reverse() {
-      int start = 0;
-      int end = grades.length - 1;
-
-      while (start < end) {
-         T temp = grades[start];
-         grades[start] = grades[end];
-         grades[end] = temp;
-
-         start++;
-         end--;
-      }
+      Algorithms.reverse(grades);
    }
 
    public void sort() {
-      boolean swapped;
-      do {
-         swapped = false;
-         for (int i = 1; i < grades.length; i++) {
-            if (grades[i - 1].compareTo(grades[i]) > 0) {
-               T temp = grades[i - 1];
-               grades[i - 1] = grades[i];
-               grades[i] = temp;
-               swapped = true;
-            }
-         }
-      } while (swapped);
+      Algorithms.sort(grades);
    }
 
    public T[] getArray() {
-      return grades.clone();
+      return grades;
    }
 }
