8a0fb147f0b4eab005ab26de74f776f26131acde
KAIWEN ZHANG <126432067+3192480025@users.noreply.github.com>
2024-03-31 19:02:09 +0800

    Algorithms.java

--- 01-arrays/01-arrays/src/main/java/oy/tol/tra/Algorithms.java
+++ 01-arrays/01-arrays/src/main/java/oy/tol/tra/Algorithms.java
@@ -1,15 +1,22 @@
 package oy.tol.tra;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-
 public class Algorithms {
-
     public static <T extends Comparable<T>> void sort(T[] array) {
-        List<T> list = Arrays.asList(array);
-        Collections.sort(list);
-        list.toArray(array);
+        int n = array.length;
+        boolean swapped;
+
+        do {
+            swapped = false;
+            for (int i = 1; i < n; i++) {
+                if (array[i - 1].compareTo(array[i]) > 0) {
+                    // Swap elements if they are in the wrong order
+                    T temp = array[i - 1];
+                    array[i - 1] = array[i];
+                    array[i] = temp;
+                    swapped = true;
+                }
+            }
+        } while (swapped);
     }
 
     public static <T> void reverse(T[] array) {
